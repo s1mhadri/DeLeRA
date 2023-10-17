d5d42dbb8a02890bacb9b3385cf966e57940df6c
Simhadri <simhadri1998@gmail.com>
2023-10-18 01:16:15 +0200

    hptune update with new dataset loader

--- hptune.py
+++ hptune.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import wandb
 from sklearn.model_selection import train_test_split
 from torch_geometric.loader import DataLoader
@@ -6,9 +8,9 @@ import torch.nn as nn
 import torch.optim as optim
 
 import config as cfg
-from input_pipeline.graph_dataloader import Temporal_Graph_Dataset
+from input_pipeline.graph_dataloader import Temporal_Graph_Dataset, Balanced_Dataset
 from input_pipeline.balance_dataset import Data_Balancer
-from models.graph_models import GCN, STGCNLSTM
+from models.graph_models import STGCNLSTM, STGCN4LSTM
 from trainer import Trainer, create_train_loss_graph
 from evaluate import Evaluate_Model
 
@@ -17,12 +19,12 @@ def train_func():
     wandb.init()
     wbconfigs = wandb.config
 
-    dataset_configs = {
-        "WIN_SHIFT": wbconfigs.WIN_SHIFT,
-        "WIN_SIZE_IN": wbconfigs.WIN_SIZE_IN,
-        "WIN_SIZE_OUT": wbconfigs.WIN_SIZE_OUT,
-    }
-    dataset = Temporal_Graph_Dataset(dataset_configs)
+    model_path_dir = cfg.model_path.rsplit("/", 1)[0]
+    Path(model_path_dir).mkdir(parents=True, exist_ok=True)
+
+    dataset_path = f"Data/processed_bal/dataset-{wbconfigs.WIN_SIZE_IN}-{wbconfigs.WIN_SIZE_OUT}-{wbconfigs.WIN_SHIFT}.pt"
+    bal_dataloader = Balanced_Dataset(dataset_path)
+    dataset, class_weights, class_samples = bal_dataloader.load_balanced_dataset()
 
     # split the dataset into train, val, and test
     train, test_data = train_test_split(dataset, test_size=0.2, random_state=42)
@@ -42,10 +44,13 @@ def train_func():
         "WIN_SIZE_OUT": wbconfigs.WIN_SIZE_OUT,
         "HIDDEN_DIM_1": wbconfigs.HIDDEN_DIM_1,
         "HIDDEN_DIM_2": wbconfigs.HIDDEN_DIM_2,
+        "NUM_LAYERS": wbconfigs.NUM_LAYERS,
+        "DROPOUT_RATE": wbconfigs.DROPOUT_RATE,
     }
-    model = STGCNLSTM(model_configs)
+    model = STGCN4LSTM(model_configs)
     # Define loss function and optimizer
-    criterion = nn.CrossEntropyLoss()
+    class_weights = torch.tensor(class_weights)
+    criterion = nn.CrossEntropyLoss(weight=class_weights.to(cfg.device))
     optimizer = optim.Adam(model.parameters(), lr=wbconfigs.LEARNING_RATE)
 
     # Train the model from scratch
@@ -74,26 +79,14 @@ def train_func():
 sweep_config = {
     "name": "DeLeRA-hptune",
     "parameters": {
-        "WIN_SIZE_IN": {
-            "distribution": "int_uniform",
-            "min": 15,
-            "max": 100,
-        },
-        "WIN_SIZE_OUT": {
-            "distribution": "int_uniform",
-            "min": 5,
-            "max": 10,
-        },
-        "WIN_SHIFT": {
-            "distribution": "int_uniform",
-            "min": 1,
-            "max": 5,
-        },
+        "WIN_SIZE_IN": {"values": [25, 35, 50, 60, 75, 90]},
+        "WIN_SIZE_OUT": {"values": [5, 10, 15]},
+        "WIN_SHIFT": {"values": [1, 3, 5]},
         "BATCH_SIZE": {"values": [16, 32, 64, 128]},
         "MAX_EPOCHS": {
             "distribution": "int_uniform",
-            "min": 5,
-            "max": 10,
+            "min": 10,
+            "max": 75,
         },
         "LEARNING_RATE": {
             "distribution": "log_uniform_values",
@@ -102,10 +95,10 @@ sweep_config = {
         },
         "HIDDEN_DIM_1": {"values": [16, 32, 64, 128]},
         "HIDDEN_DIM_2": {"values": [16, 32, 64, 128]},
-        "NUM_LAYERS": {"values": [2, 3, 4]},
+        "NUM_LAYERS": {"values": [1, 2, 3, 4]},
         "DROPOUT_RATE": {
             "distribution": "uniform",
-            "min": 0.1,
+            "min": 0,
             "max": 0.4,
         },
     },
@@ -117,4 +110,4 @@ sweep_config = {
 sweep_id = wandb.sweep(sweep_config, project="DeLeRA")
 
 # Perfrom the sweep
-wandb.agent(sweep_id, function=train_func, count=1)
+wandb.agent(sweep_id, function=train_func, count=100)
